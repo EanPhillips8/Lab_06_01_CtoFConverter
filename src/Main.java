f1fdb1c0930de8feea900986c0afb811d3c1fc4c
EanPhillips8 <eanphillips8@gmail.com>
2022-09-29 15:19:42 -0400

    ezsxdvghbn

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+
+        double temp = 0;
+        int constant = 32;
+        String Trash = "";
+
+          System.out.print("\n What is the temperature you wish to convert in Celsius: ");
+          if(in.hasNextDouble())
+          {
+              temp = in.nextDouble();
+              in.nextLine();
+              System.out.println("Your temperature in Fahrenheit is " + ((temp * 1.8) + constant));
+          }
+          else
+        {
+            Trash = in.nextLine();
+            System.out.println(Trash + " is not a number, please input a number:");
+        }
     }
 }
